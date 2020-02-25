5082d3dcc386d29487f0e200331d996ba994ca93
iamtyrique <tyriquewebley@gmail.com>
2020-02-25 10:02:58 -0500

    modified achievements controller

--- controller/Achievement/create.js
+++ controller/Achievement/create.js
@@ -1,23 +1,23 @@
 let Achievement = require('../../models/achievements');
 let Junc_user_achievement = require('../../models/junc_user_achievements');
 
-exports.createAchievement = async (req,res) => {
-    try{
+exports.createAchievement = async (req, res) => {
+    try {
         let achievement = await Achievement.create({
             name: req.body.name,
-            description: req.body.description     
+            description: req.body.description
         });
 
         res.status(200).send({
-            succes: true,
+            success: true,
             data: null,
             error: null,
             message: "Achievement created successfully!"
         })
-    }catch(err){
+    } catch (err) {
         console.error(err);
         res.status(500).send({
-            succes: false,
+            success: false,
             data: null,
             error: err,
             message: "Server could not proceess!"
